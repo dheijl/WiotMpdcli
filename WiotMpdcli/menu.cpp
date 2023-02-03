0603d31a26dd014759b9f180c33ff1d17e530f9c
dheijl <danny.heijl@telenet.be>
2023-02-03 14:13:28 +0100

    limit players to 5 and favourites to 50

--- WiotMpdcli/menu.cpp
+++ WiotMpdcli/menu.cpp
@@ -1,160 +1,155 @@
 #include "menu.h"
 #include "config.h"
 
-static void display_menuline(const MENULINE* line, uint16_t color)
-{
-    tft_write(line->x, line->y, color, String(line->text));
+static void display_menuline(const MENULINE* line, uint16_t color) {
+  tft_write(line->x, line->y, color, String(line->text));
 }
 
-static int display_menu(const vector<MENULINE*> menu)
-{
-    tft_clear();
-    int selected = 0;
-    bool repaint = true;
-    while (true) {
-        if (repaint) {
-            repaint = false;
-            int i = 0;
-            for (auto l : menu) {
-                if (i++ == selected) {
-                    display_menuline(l, TFT_GREENYELLOW);
-                } else {
-                    display_menuline(l, TFT_WHITE);
-                }
-            }
+static int display_menu(const vector<MENULINE*> menu) {
+  tft_clear();
+  int selected = 0;
+  bool repaint = true;
+  while (true) {
+    if (repaint) {
+      repaint = false;
+      int i = 0;
+      for (auto l : menu) {
+        if (i++ == selected) {
+          display_menuline(l, TFT_GREENYELLOW);
+        } else {
+          display_menuline(l, TFT_WHITE);
         }
-        int down_time = 400;
-        if (digitalRead(WIO_5S_UP) == LOW) {
-            while ((digitalRead(WIO_5S_UP) == LOW) && (down_time-- > 0)) {
-                delay(1);
-            }
-            selected -= 1;
-            if (selected < 0) {
-                selected = menu.size() - 1;
-            }
-            repaint = true;
-            continue;
-        }
-        if (digitalRead(WIO_5S_DOWN) == LOW) {
-            while ((digitalRead(WIO_5S_DOWN) == LOW) && (down_time-- > 0)) {
-                delay(1);
-            }
-            selected += 1;
-            if (selected > (menu.size() - 1)) {
-                selected = 0;
-            }
-            repaint = true;
-            continue;
-        }
-        if (digitalRead(WIO_5S_RIGHT) == LOW) {
-            while ((digitalRead(WIO_5S_RIGHT) == LOW) && (down_time-- > 0)) {
-                delay(1);
-            }
-            return selected;
-        }
-        if (digitalRead(WIO_5S_LEFT) == LOW) {
-            return -1;
-        }
+      }
+    }
+    int down_time = 400;
+    if (digitalRead(WIO_5S_UP) == LOW) {
+      while ((digitalRead(WIO_5S_UP) == LOW) && (down_time-- > 0)) {
         delay(1);
+      }
+      selected -= 1;
+      if (selected < 0) {
+        selected = menu.size() - 1;
+      }
+      repaint = true;
+      continue;
     }
+    if (digitalRead(WIO_5S_DOWN) == LOW) {
+      while ((digitalRead(WIO_5S_DOWN) == LOW) && (down_time-- > 0)) {
+        delay(1);
+      }
+      selected += 1;
+      if (selected > (menu.size() - 1)) {
+        selected = 0;
+      }
+      repaint = true;
+      continue;
+    }
+    if (digitalRead(WIO_5S_RIGHT) == LOW) {
+      while ((digitalRead(WIO_5S_RIGHT) == LOW) && (down_time-- > 0)) {
+        delay(1);
+      }
+      return selected;
+    }
+    if (digitalRead(WIO_5S_LEFT) == LOW) {
+      return -1;
+    }
+    delay(1);
+  }
 }
 
-static void select_player()
-{
-    auto players = get_config().mpd_players;
-    vector<MENULINE*> player_menu;
-    uint16_t pos = 40;
-    for (auto p : players) {
-        MENULINE* m = new MENULINE { 4, pos, p->player_name };
-        pos += 40;
-        player_menu.push_back(m);
-    }
-    MENULINE* ret = new MENULINE { 4, pos, "Return" };
-    player_menu.push_back(ret);
-    int selected = display_menu(player_menu);
-    if ((selected >= 0) && (selected < player_menu.size())) {
-        auto ip = players[selected]->player_ip;
-        tft_clear();
-        tft_println("New player @" + String(ip));
-        write_current_player(get_config(), selected);
-    }
-    for (auto ml = player_menu.begin(); ml != player_menu.end(); ++ml) {
-        delete *ml;
-    }
-    player_menu.clear();
-    delay(500);
-}
-
-static void select_favourite(int page)
-{
-    vector<MENULINE*> fav_menu;
-    uint16_t pos = 15;
-    int ifrom = page * 10;
-    int ito = ifrom + 10;
-    CONFIG& config = get_config();
-    for (int i = ifrom; i < ito; i++) {
-        if (i < config.favourites.size()) {
-            MENULINE* m = new MENULINE { 4, pos, config.favourites[i]->fav_name };
-            pos += 20;
-            fav_menu.push_back(m);
-        }
-    }
-    MENULINE* ret = new MENULINE { 4, pos, "Return" };
-    fav_menu.push_back(ret);
-    int selected = display_menu(fav_menu);
-    if ((selected >= 0) && (selected < fav_menu.size())) {
-        selected += ifrom;
-        auto url = config.favourites[selected]->fav_url;
-        auto name = config.favourites[selected]->fav_name;
-        tft_clear();
-        tft_println_highlight("Playing " + String(name));
-        play_favourite(url);
-    }
-    for (auto ml = fav_menu.begin(); ml != fav_menu.end(); ++ml) {
-        delete *ml;
-    }
-    fav_menu.clear();
-}
-
-void show_menu()
-{
-
-    static const char* mlines[] {
-        "Select Player",
-        "Favourites 1",
-        "Favourites 2",
-        "Favourites 3",
-        "Favourites 4",
-        "Favourites 5",
-        "Return"
-    };
-
+static void select_player() {
+  auto players = get_config().mpd_players;
+  vector<MENULINE*> player_menu;
+  uint16_t pos = 40;
+  for (auto p : players) {
+    MENULINE* m = new MENULINE{ 4, pos, p->player_name };
+    pos += 40;
+    player_menu.push_back(m);
+  }
+  MENULINE* ret = new MENULINE{ 4, pos, "Return" };
+  player_menu.push_back(ret);
+  int selected = display_menu(player_menu);
+  if ((selected >= 0) && (selected < player_menu.size())) {
+    auto ip = players[selected]->player_ip;
     tft_clear();
-    digitalWrite(LCD_BACKLIGHT, HIGH);
-    CONFIG& config = get_config();
-    int nfavs = config.favourites.size();
-    int npages = (npages % 10) == 0 ? (nfavs / 10) : (nfavs / 10) + 1;
-    npages = min(npages, 5);
-    vector<MENULINE*> main_menu;
-    int x = 4;
-    int y = 20;
-    for (int i = 0; i <= npages; i++) {
-        main_menu.push_back(new MENULINE { x, y, mlines[i] });
-        y += 30;
-    }
-    main_menu.push_back(new MENULINE { x, y, mlines[6] });
-    int selected = display_menu(main_menu);
-    if (selected == 0) {
-        select_player();
-    } else if (selected <= npages) {
-        select_favourite(selected - 1);
-    }
-    for (auto ml = main_menu.begin(); ml != main_menu.end(); ++ml) {
-        delete *ml;
-    }
-    main_menu.clear();
+    tft_println("New player @" + String(ip));
+    write_current_player(get_config(), selected);
+  }
+  for (auto ml = player_menu.begin(); ml != player_menu.end(); ++ml) {
+    delete *ml;
+  }
+  player_menu.clear();
+  delay(500);
+}
 
-    delay(500);
-    digitalWrite(LCD_BACKLIGHT, LOW);
+static void select_favourite(int page) {
+  vector<MENULINE*> fav_menu;
+  uint16_t pos = 15;
+  int ifrom = page * 10;
+  int ito = ifrom + 10;
+  CONFIG& config = get_config();
+  for (int i = ifrom; i < ito; i++) {
+    if (i < config.favourites.size()) {
+      MENULINE* m = new MENULINE{ 4, pos, config.favourites[i]->fav_name };
+      pos += 20;
+      fav_menu.push_back(m);
+    }
+  }
+  MENULINE* ret = new MENULINE{ 4, pos, "Return" };
+  fav_menu.push_back(ret);
+  int selected = display_menu(fav_menu);
+  if ((selected >= 0) && (selected < fav_menu.size())) {
+    selected += ifrom;
+    auto url = config.favourites[selected]->fav_url;
+    auto name = config.favourites[selected]->fav_name;
     tft_clear();
+    tft_println_highlight("Playing " + String(name));
+    play_favourite(url);
+  }
+  for (auto ml = fav_menu.begin(); ml != fav_menu.end(); ++ml) {
+    delete *ml;
+  }
+  fav_menu.clear();
+}
+
+void show_menu() {
+
+  static const char* mlines[]{
+    "Select Player",
+    "Favourites 1",
+    "Favourites 2",
+    "Favourites 3",
+    "Favourites 4",
+    "Favourites 5",
+    "Return"
+  };
+
+  tft_clear();
+  digitalWrite(LCD_BACKLIGHT, HIGH);
+  CONFIG& config = get_config();
+  int nfavs = config.favourites.size();
+  int npages = (npages % 10) == 0 ? (nfavs / 10) : (nfavs / 10) + 1;
+  npages = min(npages, 5);
+  vector<MENULINE*> main_menu;
+  int x = 4;
+  int y = 20;
+  for (int i = 0; i <= npages; i++) {
+    main_menu.push_back(new MENULINE{ x, y, mlines[i] });
+    y += 30;
+  }
+  main_menu.push_back(new MENULINE{ x, y, mlines[6] });
+  int selected = display_menu(main_menu);
+  if (selected == 0) {
+    select_player();
+  } else if (selected <= npages) {
+    select_favourite(selected - 1);
+  }
+  for (auto ml = main_menu.begin(); ml != main_menu.end(); ++ml) {
+    delete *ml;
+  }
+  main_menu.clear();
+
+  delay(500);
+  digitalWrite(LCD_BACKLIGHT, LOW);
+  tft_clear();
 }
--- WiotMpdcli/mpd_commands.cpp
+++ WiotMpdcli/mpd_commands.cpp
@@ -3,78 +3,73 @@
 static string MPD_HOST = "";
 static const int MPD_PORT = 6600;
 
-static MPD_PLAYER* get_mpd()
-{
-    digitalWrite(LCD_BACKLIGHT, HIGH);
-    if (start_wifi(get_config())) {
-        CONFIG& config = get_config();
-        MPD_PLAYER* mpd = config.mpd_players[config.active_player];
-        tft_println("Player: " + String(mpd->player_ip));
-        return mpd;
-    } else {
-        tft_println("Can't connect.");
-        return NULL;
-    }
+static MPD_PLAYER* get_mpd() {
+  digitalWrite(LCD_BACKLIGHT, HIGH);
+  if (start_wifi(get_config())) {
+    CONFIG& config = get_config();
+    MPD_PLAYER* mpd = config.mpd_players[config.active_player];
+    tft_println("Player: " + String(mpd->player_ip));
+    return mpd;
+  } else {
+    tft_println("Can't connect.");
+    return NULL;
+  }
 }
 
-static void disconnect_mpd()
-{
-    digitalWrite(LCD_BACKLIGHT, LOW);
-    tft_clear();
+static void disconnect_mpd() {
+  digitalWrite(LCD_BACKLIGHT, LOW);
+  tft_clear();
 }
 
-void toggle_mpd_status()
-{
-    auto player = get_mpd();
-    if (player != NULL) {
-        MpdConnection con;
-        if (con.Connect(player->player_ip, player->player_port)) {
-            if (con.IsPlaying()) {
-                tft_println("Stop playing");
-                con.Stop();
-            } else {
-                tft_println("Start playing");
-                con.Play();
-            }
-            con.Disconnect();
-        }
-        delay(2000);
-        while (digitalRead(WIO_KEY_A) == LOW) {
-            delay(100);
-        }
-        disconnect_mpd();
+void toggle_mpd_status() {
+  auto player = get_mpd();
+  if (player != NULL) {
+    MpdConnection con;
+    if (con.Connect(player->player_ip, player->player_port)) {
+      if (con.IsPlaying()) {
+        tft_println("Stop playing");
+        con.Stop();
+      } else {
+        tft_println("Start playing");
+        con.Play();
+      }
+      con.Disconnect();
     }
+    delay(2000);
+    while (digitalRead(WIO_KEY_A) == LOW) {
+      delay(100);
+    }
+    disconnect_mpd();
+  }
 }
 
-void show_mpd_status()
-{
-    auto player = get_mpd();
-    if (player != NULL) {
-        MpdConnection con;
-        if (con.Connect(player->player_ip, player->player_port)) {
-            con.GetStatus();
-            con.GetCurrentSong();
-            con.Disconnect();
-        }
-        delay(3000);
-        while (digitalRead(WIO_KEY_A) == LOW) {
-            delay(100);
-        }
-        disconnect_mpd();
+void show_mpd_status() {
+  auto player = get_mpd();
+  if (player != NULL) {
+    MpdConnection con;
+    if (con.Connect(player->player_ip, player->player_port)) {
+      con.GetStatus();
+      con.GetCurrentSong();
+      con.Disconnect();
     }
+    delay(3000);
+    while (digitalRead(WIO_KEY_A) == LOW) {
+      delay(100);
+    }
+    disconnect_mpd();
+  }
 }
 
-void play_favourite(const char* url)
-{
-    auto player = get_mpd();
-    if (player != NULL) {
-        MpdConnection con;
-        if (con.Connect(player->player_ip, player->player_port)) {
-            con.Clear();
-            con.Add_Url(url);
-            con.Play();
-        }
-        delay(2000);
-        disconnect_mpd();
+void play_favourite(const char* url) {
+  auto player = get_mpd();
+  if (player != NULL) {
+    MpdConnection con;
+    if (con.Connect(player->player_ip, player->player_port)) {
+      con.Clear();
+      con.Add_Url(url);
+      con.Play();
     }
+    delay(2000);
+    disconnect_mpd();
+  }
 }
